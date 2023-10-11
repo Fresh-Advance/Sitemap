a0e7eb1d4db97df0563ed483c449e7ea19a3d869
Anton Fedurtsya <anton@fedurtsya.com>
2023-10-11 22:58:48 +0300

    Add sitemap data table
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ migration/data/Version20231011192842.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FreshAdvance\Sitemap\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231011192842 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            "CREATE TABLE fa_sitemap (
+                id INT AUTO_INCREMENT,
+                object_id VARCHAR(32),
+                object_type VARCHAR(32),
+                location VARCHAR(255),
+                modified VARCHAR(30),
+                frequency VARCHAR(10),
+                priority FLOAT,
+                updated TINYINT(1),
+                added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (id),
+                UNIQUE KEY unique_object (object_id, object_type),
+                INDEX idx_object_id (object_id)
+            );"
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
